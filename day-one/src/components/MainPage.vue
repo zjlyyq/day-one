52cf14678ab4803c68b9ecf5a01b852cf3915f0e
jialuzhang <jialuzhang0805@gmail.com>
2019-03-04 07:40:46 +0800

    init

---
+++ day-one/src/components/MainPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <!-- <div class="container">
+            <div class="row">
+                <div class="col-md-4">拍照</div>
+                <div class="col-md-4">语音</div>
+                <div class="col-md-4">新建</div>
+            </div>
+            <div class="row">
+                <div class="col-xs-4 col-md-6">拍照</div>
+                <div class="col-xs-4 col-md-3">语音</div>
+                <div class="col-xs-4 col-md-3">新建</div>
+            </div>
+        </div> -->
+        <header>
+            <div><img src="../assets/pic.png"></div>
+            <div><img src="../assets/mic2.png"></div>
+            <div><img src="../assets/edit.png"></div>
+        </header>
+    </div>
+</template>
+
+<style>
+    .container {
+        background: #eee;
+    }
+    
+    header {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        height: 330px;
+        background: #5e9ecc;
+        color: #fff;
+    }
+    header img{
+        height:48px;
+        width:48px;
+    }
+</style>
